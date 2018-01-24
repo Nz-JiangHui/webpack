37b2eb98042aa4904af273b06ca3c7bb2eb2e23f
jianghui <nz.jianghui@gmail.com>
2018-01-24 18:28:24 +0800

    root

--- release.js
+++ release.js
@@ -6,7 +6,7 @@ var Client = require('ssh2').Client;
 var conn = new Client();
 conn.on('ready', function() {
     console.log('Client :: ready');
-    conn.exec('cd jianghui/webpack && git pull && ./release.sh', function(err, stream) {
+    conn.exec('cd /root/home/jianghui/webpack && git pull && ./release.sh', function(err, stream) {
         if (err) throw err;
         stream.on('close', function(code, signal) {
             console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
