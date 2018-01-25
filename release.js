cbce7770a195678669d17529debc68823406c7a0
jianghui <nz.jianghui@gmail.com>
2018-01-25 15:08:37 +0800

    rel

--- release.js
+++ release.js
@@ -6,7 +6,7 @@ var Client = require('ssh2').Client;
 var conn = new Client();
 conn.on('ready', function() {
     console.log('Client :: ready');
-    conn.exec('cd jianghui/webpack && ./release.sh', function(err, stream) {
+    conn.exec('./release.sh', function(err, stream) {
         if (err) throw err;
         stream.on('close', function(code, signal) {
             console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
--- release.sh
+++ release.sh
@@ -2,4 +2,4 @@
 . ~/.nvm/nvm.sh
 . ~/.profile
 . ~/.bashrc
-git pull && git checkout master && git pull && npm run stop && npm run run
+cd jianghui/webpack && git pull && git checkout master && git pull && npm run stop && npm run run
